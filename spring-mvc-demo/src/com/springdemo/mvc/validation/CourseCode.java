d0190ac86a08a63f52bbcfc8724af3b1deab66ed
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-12-18 18:09:41 +1000

    Creating a Custom Java Annotation

---
+++ spring-mvc-demo/src/com/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,30 @@
+package com.springdemo.mvc.validation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ FIELD, METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+	
+	// define default course code
+	public String value() default "DIV";
+	
+	// define default error message
+	public String message() default "must start with DIV";
+	
+	// define default groups
+	public Class<?>[] groups() default {};
+	
+	// define default payloads
+	public Class<? extends Payload>[] payload() default {};
+
+}
