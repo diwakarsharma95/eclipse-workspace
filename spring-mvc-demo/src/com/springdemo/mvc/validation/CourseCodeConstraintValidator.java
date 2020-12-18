cbecce7c88cdb82896f99ee92c5f012f77e95194
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-12-18 18:32:10 +1000

    Developing the ConstraintValidator

---
+++ spring-mvc-demo/src/com/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,22 @@
+package com.springdemo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+	
+	private String coursePrefix;
+	
+	@Override
+	public void initialize(CourseCode theCourseCode) {
+		coursePrefix = theCourseCode.value();
+	}
+
+	@Override
+	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+		// TODO Auto-generated method stub
+		boolean result = theCode.startsWith(coursePrefix);
+		return result;
+	}
+
+}
